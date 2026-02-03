58dfe89a1b7ba6ec3288936d30550d6cf58ba7e9
proemian <brian@proemial.ai>
2026-02-03 20:05:20 +0100

    Fix android build

--- src-tauri/gen/android/app/src/main/java/chat/eddie/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/chat/eddie/MainActivity.kt
@@ -1,12 +1,10 @@
-package chat.eddie.app
+package chat.eddie
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    // Switch from splash theme to regular app theme
-    setTheme(R.style.Theme_eddie_chat)
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
   }
